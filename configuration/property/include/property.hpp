6e229c07013c39371ca44614b72e94e811496438
johann-gorban <ivangorbanwork@gmail.com>
2025-06-14 13:44:03 +0300

    Main namespace renamed, value renamed to propery

---
+++ configuration/property/include/property.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <string>
+
+#include "processing/utils/token.hpp"
+
+class token_ptr;
+
+namespace conf {
+
+class Value {
+private:    
+    const std::string data_;
+public:
+    explicit Value(const token_ptr token);
+public:
+    int to_int() const;
+    
+    bool to_bool() const;
+    
+    float to_float() const;
+
+    std::string to_string() const;
+};
+
+class IntegerValue : public Value {
+public:
+    explicit IntegerValue(const token_ptr token);
+public:
+    int to_int() const;
+};
+
+class FloatValue : public Value {
+public:
+    explicit FloatValue(const token_ptr token);
+public:
+    float to_float() const;
+};
+
+class BoolValue : public Value {
+public:
+    explicit BoolValue(const token_ptr token);
+public:
+    bool to_bool() const;
+};
+
+class StringValue : public Value {
+public:
+    explicit StringValue(const token_ptr token);
+public:
+    std::string to_string() const;
+};
+
+};
