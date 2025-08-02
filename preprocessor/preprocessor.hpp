4511108680eca0361f0cebfb3238bd57572f7471
der-johanni <ivangorbanwork@gmail.com>
2025-08-02 21:44:37 +0300

    Preprocessor first version released

--- preprocessor/preprocessor.hpp
+++ preprocessor/preprocessor.hpp
@@ -5,9 +5,18 @@
 
 namespace config {
 
-    class Preprocessor {
-    public:
-        static std::vector<std::string> process(const std::string& filepath);
-    }
+class Preprocessor {
+public:
+    static std::vector<std::string> process(const std::string& filepath);
+
+private:
+    static std::vector<std::string> read_lines(const std::string& filepath);
+
+    static std::string remove_comment(const std::string& line);
+
+    static std::string remove_space(const std::string& str);
+
+    static std::vector<std::string> join_block(const std::vector<std::string>& lines);
+};
 
 }
